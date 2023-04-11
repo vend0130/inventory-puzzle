c237b2df9d6689ab9b139a18d527824d48719005
vend <8vend8@gmail.com>
2023-04-12 00:12:23 +0900

    small fix

--- Assets/Code/Game/ItemInfo/MenuButtonData.cs
+++ Assets/Code/Game/ItemInfo/MenuButtonData.cs
@@ -2,38 +2,22 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 namespace Code.Game.ItemInfo
 {
     [Serializable]
-    public class MenuButtonData : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
+    public class MenuButtonData : MonoBehaviour, IPointerDownHandler
     {
         [field: SerializeField] public TextMeshProUGUI Text { get; private set; }
-
-        [SerializeField] private Image _current;
-        [SerializeField] private Color _defaultColor;
-        [SerializeField] private Color _enterColor;
 
         public event Action<int> DownHandler;
 
         private int _index;
 
-        private void OnDisable() =>
-            _current.color = _defaultColor;
-
-        public void OnPointerDown(PointerEventData eventData) =>
+        public void OnPointerDown(PointerEventData _) =>
             DownHandler?.Invoke(_index);
 
-        public void OnPointerEnter(PointerEventData eventData) =>
-            _current.color = _enterColor;
-
-        public void OnPointerExit(PointerEventData eventData) =>
-            _current.color = _defaultColor;
-
-        public void ChangeIndex(int index)
-        {
+        public void ChangeIndex(int index) =>
             _index = index;
-        }
     }
 }
---
+++ Assets/Code/UI/EnterImageColoring.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Code.UI
+{
+    public class EnterImageColoring : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] private Image _current;
+        [SerializeField] private Color _defaultColor;
+        [SerializeField] private Color _enterColor;
+        
+        private void OnDisable() =>
+            _current.color = _defaultColor;
+
+        public void OnPointerEnter(PointerEventData eventData) =>
+            _current.color = _enterColor;
+
+        public void OnPointerExit(PointerEventData eventData) =>
+            _current.color = _defaultColor;
+    }
+}
